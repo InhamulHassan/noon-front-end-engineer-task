cbca0b2a347b0289249461438b8a27d42288c02a
Inhamul Hassan <inhamul.h@blackvt.com>
2022-02-06 00:35:31 +0530

    Integrated app wide layout

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,24 +1,17 @@
-import { ThemeProvider } from "styled-components";
 import type { AppProps } from 'next/app'
+
+// layout
+import Layout from "../components/Layout";
 
 // global styles
 import GlobalStyles from '../styles/global'
 
-
-const theme = {
-  colors: {
-    primary: "#fafafa",
-  },
-};
-
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <>
+    <Layout>
       <GlobalStyles />
-      <ThemeProvider theme={theme}>
-        <Component {...pageProps} />
-      </ThemeProvider>
-    </>
+      <Component {...pageProps} />
+    </Layout>
   );
 }
 
