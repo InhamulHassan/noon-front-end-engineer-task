266a20f39defaf68a8a1839395cd3873f81521d1
Inhamul Hassan <inhamul.h@blackvt.com>
2022-02-05 21:41:05 +0530

    Added logo link

--- components/Navigation.tsx
+++ components/Navigation.tsx
@@ -49,6 +49,7 @@ const NavLogo = styled.img`
     width: auto;
     margin: 5px 15px 5px 0;
     object-fit: contain;
+    cursor: pointer;
 
     /* on screens smaller than 640px (mobiles/mini tablets) */
     @media only screen and (max-width: 640px) {
@@ -149,12 +150,13 @@ export const Navigation: React.FC = () => {
     return (
         <NavContainer>
             <NavInnerContainer>
-                <NavLogo src='/assets/ideaa-logotype.png'></NavLogo>
+                <Link href='/' passHref>
+                    <NavLogo src='/assets/ideaa-logotype.png'></NavLogo>
+                </Link>
                 <NavLinkList>
                     <NavLinkItem>
                         <Link href='/' passHref>
                             <NavLinkElement className={route == '/' ? 'selected' : ''}>
-
                                 <NavLinkIconHome viewBox="0 0 24 24" width="24" height="24">
                                     <path fillRule="evenodd" d="M11.03 2.59a1.5 1.5 0 011.94 0l7.5 6.363a1.5 1.5 0 01.53 1.144V19.5a1.5 1.5 0 01-1.5 1.5h-5.75a.75.75 0 01-.75-.75V14h-2v6.25a.75.75 0 01-.75.75H4.5A1.5 1.5 0 013 19.5v-9.403c0-.44.194-.859.53-1.144l7.5-6.363zM12 3.734l-7.5 6.363V19.5h5v-6.25a.75.75 0 01.75-.75h3.5a.75.75 0 01.75.75v6.25h5v-9.403L12 3.734z"></path>
                                 </NavLinkIconHome>
